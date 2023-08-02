31c270ebaa3c479463cecaab29ad480057ad7f6a
mohammad alsaadi <moalsaadiformal@gmail.com>
2023-08-02 13:15:07 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,52 @@ import 'package:task1/screens/addPage.dart';
 import 'package:task1/screens/auth/login_page.dart';
 import 'package:task1/screens/getStarted.dart';
 import 'package:task1/screens/homePage.dart';
-import 'package:task1/service/currentUser.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
+  Future<String> getUUID() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String uuid = prefs.getString('currentUserId') ?? "";
+    return uuid;
+  }
 
   @override
   Widget build(BuildContext context) {
-    CurrentUser currentUser = CurrentUser();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: (currentUser.currentUserId != null)
-          ? HomePage(
-              currentUserID: currentUser.currentUserId,
-            )
-          : const GetStarted(),
+      home: FutureBuilder<String>(
+        future: getUUID(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            // Handle the error case here
+            return Scaffold(
+              body: Center(
+                child: Text('Error: ${snapshot.error}'),
+              ),
+            );
+          } else {
+            String? currentUserId = snapshot.data;
+            if (currentUserId != null && currentUserId.isNotEmpty) {
+              // If the currentUserId is not null or empty, show the HomePage
+              return HomePage(currentUserID: currentUserId);
+            } else {
+              // If the currentUserId is null or empty, show the GetStarted page
+              return GetStarted();
+            }
+          }
+        },
+      ),
       onGenerateRoute: (settings) {
         if (settings.name == '/AddPage') {
           return MaterialPageRoute(
@@ -33,6 +60,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 /*
 
 class MyApp extends StatelessWidget {
